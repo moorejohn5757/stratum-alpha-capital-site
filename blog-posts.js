2fc9d36e79e901d70d3b56118709f1b65ce92cb7
John Moore <johnmoore@Johns-Laptop.local>
2026-05-02 16:08:06 -0400

    Make blog page data-driven

---
+++ blog-posts.js
@@ -0,0 +1,12 @@
+window.STRATUM_BLOG_POSTS = [
+  {
+    date: "May 2026",
+    title: "Using This Page",
+    summary: "This page can be updated privately by editing one source file in the website repo.",
+    body: [
+      "To publish a new post, edit the blog-posts.js file in the Stratum Alpha Capital website repository and add a new object to the top of the array.",
+      "Visitors can only read the published posts. There is no public editing interface on the live site."
+    ],
+    links: []
+  }
+];
